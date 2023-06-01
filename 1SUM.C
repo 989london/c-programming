fb00c131aa787921a13198efa9db6391f71e7282
989london <135216974+989london@users.noreply.github.com>
2023-06-01 14:45:24 +0530

    Add files via upload

---
+++ 1SUM.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,c;
+	clrscr();
+
+	printf("\n enter first number:");
+	scanf("%d",&a);
+
+	printf("\n enter secound number:");
+	scanf("%d",&b);
+
+	c=a+b;
+
+	printf("\n sum is: %d",c);
+
+	getch();
+}
